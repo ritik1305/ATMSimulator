869cf44c2044e76e1589b2eec531811fee4d693f
ritik1305 <ritik1305@gmail.com>
2021-06-01 13:46:31 +0530

    First commit

---
+++ src/main/java/Code/conn.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Code;
+
+import java.sql.*;
+/**
+ *
+ * @author ritik
+ */
+public class conn {
+    
+    Connection c;
+    Statement s;
+    
+    public conn(){
+        
+    }
+    
+    
+}
